cc9f84238cea3067d40f3c18c0fd45089adf12b3
Timothée AUPETIT <timothee.aupetit@proton.com>
2024-03-05 21:31:17 +0100

    Added unit tests for 4 players games

--- src/test/scala/org/perso/GameTest.scala
+++ src/test/scala/org/perso/GameTest.scala
@@ -1,17 +1,71 @@
 package org.perso
 
+import org.perso.Contract._
 import org.scalatest.funspec.AnyFunSpec
 
 
 class GameTest extends AnyFunSpec {
-  describe("4 players game") {
-    val p1 = Player(1, "Léontine")
-    val p2 = Player(2, "Fernand")
-    val p3 = Player(3, "Adélaïde")
-    val p4 = Player(4, "Henri")
 
-    it("must give 100 points to the challenger") {
-      assert(false)
+  private val p1 = Player(1, "Octavie")
+  private val p2 = Player(2, "Fernand")
+  private val p3 = Player(3, "Ursule")
+  private val p4 = Player(4, "Hippolyte")
+
+  describe("4 players game") {
+    val players = Set(p1, p2, p3, p4)
+
+    it("sum of all scores must equal 0") {
+      val game = new Game(
+        players,
+        challenger = p4,
+        contract = Garde,
+        trickPoints = 53,
+        oudlers = 1
+      )
+
+      val scores = game.calculateScores
+
+      assert(scores.values.sum == 0)
+    }
+
+    describe("won contract") {
+      it("the challenger must win 444 points") {
+        val game = new Game(
+          players,
+          challenger = p2,
+          contract = GardeSans,
+          trickPoints = 53,
+          oudlers = 2
+        )
+
+        val scores = game.calculateScores
+
+        assert(scores(p2) == 444)
+
+        val defenderScores = scores - p2
+
+        assert(defenderScores.values.forall(_ == -148))
+      }
+    }
+
+    describe("lost contract") {
+      it("the challenger must lose 468 points") {
+        val game = new Game(
+          players,
+          challenger = p1,
+          contract = GardeContre,
+          trickPoints = 50,
+          oudlers = 1
+        )
+
+        val scores = game.calculateScores
+
+        assert(scores(p1) == -468)
+
+        val defenderScores = scores - p1
+
+        assert(defenderScores.values.forall(_ == 156))
+      }
     }
   }
 }
